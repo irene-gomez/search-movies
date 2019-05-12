ec9afb8d2bda47cfcba4fdb40cbc5787dcaca830
irenegf83 <irene@irenegf.com>
2019-05-12 18:24:46 +0200

    Create docs directory

---
+++ docs/precache-manifest.5476ae9d2247d2c7d996816e4b10e0d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0e60f4d99698dd620274207534bd08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c16272f03fc206efb12",
+    "url": "./static/css/main.92559792.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "./static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "5c16272f03fc206efb12",
+    "url": "./static/js/main.481e5990.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
